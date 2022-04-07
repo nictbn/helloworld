adf458a2e59de8e38bad3a3962bc830b3d4c447a
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-07 20:57:59 +0300

    Jar Plugin Example

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,8 @@
+public class HelloWorldTest {
+    public void testHello() {
+        System.out.println("Hello...");
+    }
+    public void testWorld() {
+        System.out.println("TEST - World!!!");
+    }
+}
